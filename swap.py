58a01acdd5418d370ad485505c559b0640bdf401
손량 <loop.infinitely@gmail.com>
2019-04-04 15:00:10 +0900

    week 1 finished

---
+++ swap.py
@@ -0,0 +1,11 @@
+a = 100
+b = 200
+print('Before')
+print(a)
+print(b)
+temp = a
+a = b
+b = temp
+print('After:')
+print(a)
+print(b)
---
+++ years.py
@@ -0,0 +1,3 @@
+year = 2019
+for i in range(year - 2, year + 3):
+    print(i)
