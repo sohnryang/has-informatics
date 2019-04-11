ef78d3e30943ccfa6421b7bf0efb4678800fc7c4
손량 <loop.infinitely@gmail.com>
2019-04-11 13:49:16 +0900

    added string formatting

--- formatting.py
+++ formatting.py
@@ -3,4 +3,8 @@ print('%5d' % 123)
 print('%05d' % 123)
 
 print('%f' % 3.141592)
-print('%3.3f' % 3.141592)
+print('%10.3f' % 3.141592)
+
+print('%s' % 'python')
+print('%10s' % 'python')
+print('%-10s' % 'python')
