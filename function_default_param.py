4163af4245e9a50cf6031451fa43b961a436e4a0
손량 <loop.infinitely@gmail.com>
2019-05-30 14:15:29 +0900

    added function with list as param

---
+++ function_default_param.py
@@ -0,0 +1,5 @@
+def plus(a=0, b=0, c=0):
+    return a + b + c
+
+print(plus(1, 1))
+print(plus(2, 3, 4))
---
+++ function_list_param.py
@@ -0,0 +1,8 @@
+def plus(ls):
+    result = 0
+    for n in ls:
+        result += n
+    return result
+
+num_ls = [1, 2, 3, 4, 5]
+print(plus(num_ls))
