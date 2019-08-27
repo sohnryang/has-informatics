f7438b45a7e9c9cb25676ea6f0144a5d4764cf1f
Ryang Sohn <loop.infinitely@gmail.com>
2019-08-27 14:31:50 +0900

    Add modules

---
+++ modules/shape.py
@@ -0,0 +1,10 @@
+def square_area(side):
+    return side ** 2
+
+
+def rect_area(side1, side2):
+    return side1 * side2
+
+
+def triangle_area(bottom_side, height):
+    return bottom_side * height / 2
---
+++ modules/shape_test.py
@@ -0,0 +1,5 @@
+import shape
+
+print(shape.square_area(2))
+print(shape.rect_area(6, 7))
+print(shape.triangle_area(6, 7))
